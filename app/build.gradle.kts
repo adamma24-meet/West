5fe07ee72e35adfbcf7f902e914712b3c0c1bc89
adamma24-meet <93448116+adamma24-meet@users.noreply.github.com>
2024-07-25 20:33:06 +0300

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services") version "4.4.2" apply false
+//    id("com.android.application")
+//    id("com.google.gms.google-services")
 }
 
 android {
@@ -62,4 +65,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp")
     implementation(libs.play.services.location)
     implementation(libs.androidx.navigation.compose)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
